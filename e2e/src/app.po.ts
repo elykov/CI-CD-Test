08c26a5306c6615831fbfe38daa946e285ab83e1
elykov <elykov_anatoly@oulook.com>
2021-01-08 00:12:14 +0200

    some changes

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,23 @@
 import { browser, by, element } from 'protractor';
 
-export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+export class LetslearnPage {
+  navigateTo() {
+    return browser.get('/');
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getTitleText() {
+    return element(by.css('h1')).getText();
+  }
+
+  getPoints() {
+    return element(by.cssContainingText('div', 'Points')).$('span').getText();
+  }
+
+  getPlus1Button() {
+    return element(by.cssContainingText('button', 'Plus 1'));
+  }
+
+  getResetButton() {
+    return element(by.cssContainingText('button', 'Reset'));
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'lsl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ci-cd-test';
+  title = 'Letslearn';
+  points = 1;
+
+  plus1() {
+    this.points++;
+  }
+
+  reset() {
+    this.points = 0;
+  }
 }
